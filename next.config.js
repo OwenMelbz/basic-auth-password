66f93e88c73613e3bb8eb834fe6d23a9d56a7724
Owen Melbourne <owen.melbourne@fnatic.com>
2022-01-04 14:34:09 +0000

    🔨 WIP - test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 const withTM = require('@vercel/edge-functions-ui/transpile')()
 
-module.exports = withTM()
+module.exports = withTM({
+    env: {
+        BASIC_AUTH: process.env.BASIC_AUTH,
+    }
+})
